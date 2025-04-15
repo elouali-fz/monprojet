5cb562731b78aaba00125ca7864866aba2b2f7b1
Youssefalamii <yalami314@gmail.com>
2025-04-15 10:35:42 +0100

    Btn Marquesl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use App\Models\Marque;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
-    }
+    public function boot()
+{
+    View::composer('*', function ($view) {
+        $view->with('marques', Marque::all());
+    });
+}
 }
